18dba20ac5fd86334e061feb6b5e05a8b14355ba
Djane Rey Mabelin <thedjaney@gmail.com>
2016-10-19 01:07:53 +0800

    add some conversations

--- brain/index.js
+++ brain/index.js
@@ -7,7 +7,8 @@ Brain.prototype.talk = function(req){
     for (var i = 0; i < this.conversations.length; i++) {
         var conv = this.conversations[i];
         var regex = new RegExp(conv.pattern,'i');
-        var reply = conv.text;
+        var rand = Math.floor((Math.random() * conv.text.length));
+        var reply = conv.text[rand];
         if(regex.test(req.text)){
             var match = regex.exec(req.text);
             for (var j = 0; j < match.length; j++) {
